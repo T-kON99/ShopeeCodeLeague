a65aece1dc1eb0339b051ef0eb51abdd2505ff97
Kenneth Anderson <eucliwood98@gmail.com>
2020-06-17 18:04:24 +0800

    added config

---
+++ config.py
@@ -0,0 +1,26 @@
+WORKSHOP_ORGANIZER = {
+    'rocketacademy': {
+        'url': 'https://rocketacademy.co/',
+        'icon': 'https://rocketacademy.co/favicon.ico'
+    },
+    'eskwelabs': {
+        'url': 'https://www.eskwelabs.com/',
+        'icon': 'https://media-exp1.licdn.com/dms/image/C4E0BAQEI0QzuDGOSdA/company-logo_200_200/0?e=1600300800&v=beta&t=e4w-h8HVkhW2DzAeZCjFEnAiYOBoN4QSfgsVD0FPtC0'
+    },
+    'codinglab': {
+        'url': 'https://www.codinglab.com.sg/',
+        'icon': 'https://www.codinglab.com.sg/favicon.ico'
+    },
+    'shopee': {
+        'url': 'https://shopee.sg/',
+        'icon': 'https://shopee.sg/favicon.ico'
+    },
+    'thebrainerycode': {
+        'url': 'https://www.thebrainerycode.com/',
+        'icon': 'https://pagevamp-uploads.s3.amazonaws.com/480113448815972/logo/logo.png?v=1519211747'
+    },
+    'generalassembly': {
+        'url': 'https://generalassemb.ly/',
+        'icon': 'https://generalassemb.ly/favicon.ico'
+    },
+}
